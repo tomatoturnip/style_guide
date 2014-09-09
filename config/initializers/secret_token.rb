4010ff1b661528a86e62548ceadb1525e792fe23
Lydia Ho <lyd.w.ho@gmail.com>
2014-09-09 17:28:21 -0400

    Created new rails app for style gallery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StyleGuide::Application.config.secret_key_base = '6a74edda47d9b050e59dc3ffcd7708c3c41ce566cba63786b49ebe04e44a4e5a717e98d9a5da1d5f649315b0815ccdfff23cc785f713ae3b4791746f89cb9af5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StyleGuide::Application.config.session_store :cookie_store, key: '_style_guide_session'
